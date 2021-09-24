9cc506cd09755df225693f2ebc29329b7767c100
Magnus Viktorsson <m.viktorsson@gmail.com>
2021-09-24 18:57:09 +0200

    formatted with prettier

--- js/logic.js
+++ js/logic.js
@@ -1,11 +1,22 @@
-import { timeObject } from "./timeObject"
+import { timeObject } from "./timeObject";
 
-const timeQuiz = new timeObject()
+const timeQuiz = new timeObject();
 
 // Question text
-const questionText = document.querySelector(".start-time-text")
-// Time to add to question 
-const timeToAdd = document.querySelector(".start-time-question")
+const questionText = document.querySelector(".start-time-text");
+// Time to add to question
+const timeToAdd = document.querySelector(".start-time-question");
 
-questionText.textContent = `Time ${(timeQuiz.startHour) }:${timeQuiz.startMinute}`;
-timeToAdd.textContent = `Add ${timeQuiz.hourToChange}h ${timeQuiz.minuteToChange}min`
+const submitButton = document.querySelector(".submit-button");
+
+const timeInput = document.querySelector(".time-input");
+
+// Set labels initally
+questionText.textContent = `Time ${timeQuiz.startHour}:${timeQuiz.startMinute}`;
+timeToAdd.textContent = `Add ${timeQuiz.hourToChange}h ${timeQuiz.minuteToChange}min`;
+
+// Continue here with label
+submitButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log(timeInput);
+});
