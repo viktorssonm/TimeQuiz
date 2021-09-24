6fd8195ba163303efd55879828e90faff0850470
Magnus Viktorsson <m.viktorsson@gmail.com>
2021-09-24 21:53:49 +0200

    add mock alert box

--- js/logic.js
+++ js/logic.js
@@ -7,16 +7,31 @@ const questionText = document.querySelector(".start-time-text");
 // Time to add to question
 const timeToAdd = document.querySelector(".start-time-question");
 
+// Check button
 const submitButton = document.querySelector(".submit-button");
 
+// Time input
 const timeInput = document.querySelector(".time-input");
 
-// Set labels initally
-questionText.textContent = `Time ${timeQuiz.startHour}:${timeQuiz.startMinute}`;
-timeToAdd.textContent = `Add ${timeQuiz.hourToChange}h ${timeQuiz.minuteToChange}min`;
-
-// Continue here with label
+// Add event listener to check button
 submitButton.addEventListener("click", (e) => {
     e.preventDefault();
-    console.log(timeInput);
+    if (timeQuiz.checkTime(timeInput.value)) {
+        alert("Well done!");
+        reloadQuestion();
+    } else {
+        alert("ERROR! Try again");
+    }
 });
+
+const reloadQuestion = () => {
+    // Reload timequiz with new task.
+    timeQuiz.reloadTask();
+
+    // Update values
+    questionText.textContent = `Time ${timeQuiz.startHour}:${timeQuiz.startMinute}`;
+    timeToAdd.textContent = `Add ${timeQuiz.hourToChange}h ${timeQuiz.minuteToChange}min`;
+    timeInput.value = "";
+};
+
+reloadQuestion();
--- js/timeObject.js
+++ js/timeObject.js
@@ -11,16 +11,37 @@ export class timeObject {
         return [hour, minute];
     };
 
+    // Function to check for correct answer with time input from HTML input type time.
+    checkTime = (timeInput) => {
+        let timeInputSplitted = timeInput.replace("0", "").split(":");
+        if (
+            timeInputSplitted[0] === String(this.correctHour) &&
+            timeInputSplitted[1] === String(this.correctMinute)
+        ) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
     reloadTask = () => {
         // Generate random start
-        this.startHour = Math.floor(Math.random() * 23);
-        this.startMinute = Math.floor(Math.random() * 59);
+        this.startHour = String(Math.floor(Math.random() * 23)).padStart(
+            2,
+            "0"
+        );
+        this.startMinute = String(Math.floor(Math.random() * 59)).padStart(
+            2,
+            "0"
+        );
+
         // Create a new date object to act as base
         let startTime = new Date();
         startTime.setHours(this.startHour);
         startTime.setMinutes(this.startMinute);
         startTime.setSeconds = 0;
 
+        // Generate random timechange
         const timeToChange = this.generateTimeToChange();
         const timeToAdjust = timeToChange[0] * 3600 + timeToChange[1] * 60;
 
@@ -38,5 +59,7 @@ export class timeObject {
         // Assign correct values to global variables.
         this.correctHour = newCorrectDate.getHours();
         this.correctMinute = newCorrectDate.getMinutes();
+
+        console.log(this.correctHour, "", this.correctMinute);
     };
 }
